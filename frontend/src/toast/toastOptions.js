c9256f3354cd31b4018cea0ab0f69cb41fa7bcc6
Alberto <info@peralstudio.com>
2021-06-20 20:39:19 +0200

    Update Toast

--- frontend/src/toast/toastOptions.js
+++ frontend/src/toast/toastOptions.js
@@ -9,7 +9,6 @@ export const notifyAdd = (name) => toast(`✔ Añadido al carrito: ${name}`, {
     draggable: true,
     progress: undefined,
     type: toast.TYPE.SUCCESS,
-
 });
 
 export const notifyRemove = (name) => toast(`❌ Eliminado del carrito: ${name}`, {
@@ -21,5 +20,15 @@ export const notifyRemove = (name) => toast(`❌ Eliminado del carrito: ${name}`
     draggable: true,
     progress: undefined,
     type: toast.TYPE.ERROR,
+});
 
+export const notifyEmptyCart = () => toast(`✔ Carrito Vaciado`, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    type: toast.TYPE.DEFAULT,
 });
